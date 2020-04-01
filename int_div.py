eb3d1095b59745dcb3f19bb676ab52cd4bc24773
Your Name <you@example.com>
2020-04-01 19:20:43 +0530

    My first commit

---
+++ int_div.py
@@ -0,0 +1,5 @@
+"""
+Python has a operator for integer division 
+"""
+print(3 // 2) # 1
+print(3/2) # 1.5
---
+++ programs/hello.py
@@ -0,0 +1,4 @@
+# first program
+print("Hello World!")
+# ctrl + S  ----> Save
+# run using: python3 file_name.py
